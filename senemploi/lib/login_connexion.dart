a4861b117405b11e96944cc3758a6ba43b6a2256
Ahmadoubambamb <mboup.ahmadou-bamba@ugb.edu.sn>
2024-10-28 12:24:07 +0000

    page de connexion

---
+++ senemploi/lib/login_connexion.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class LoginConnexion extends StatelessWidget {
+  LoginConnexion({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Connexion'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Mot de passe',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Appeler la fonction de connexion ici
+              },
+              child: const Text('Se connecter'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Naviguer vers l'écran de création de compte
+              },
+              child: const Text('Créer un compte'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

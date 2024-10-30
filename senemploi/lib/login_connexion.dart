a39f7f6695cde4753fca9582af005c52a2349456
Ahmadoubambamb <mboup.ahmadou-bamba@ugb.edu.sn>
2024-10-30 15:45:19 +0000

    ajout d'image a yaml

--- senemploi/lib/login_connexion.dart
+++ senemploi/lib/login_connexion.dart
@@ -8,46 +8,53 @@ class LoginConnexion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Connexion'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Mot de passe',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Appeler la fonction de connexion ici
-              },
-              child: const Text('Se connecter'),
-            ),
-            TextButton(
-              onPressed: () {
-                // Naviguer vers l'écran de création de compte
-              },
-              child: const Text('Créer un compte'),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Connexion'),
         ),
-      ),
-    );
+        body: ListView(children: [
+          Image.asset(
+            'images/Recrutement',
+            width: 700,
+            height: 700,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Mot de passe',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Appeler la fonction de connexion ici
+                  },
+                  child: const Text('Se connecter'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Naviguer vers l'écran de création de compte
+                  },
+                  child: const Text('Créer un compte'),
+                ),
+              ],
+            ),
+          ),
+        ]));
   }
 }
